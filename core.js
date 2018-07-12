ca2d866d254a66ca3cc538818425d7ab5fee582d
Dromarch <41089281+Dromarch@users.noreply.github.com>
2018-07-12 17:38:13 -0300

    test

--- core.js
+++ core.js
@@ -34,6 +34,13 @@ function includeHTML() {
   }
 }
 
-function changePage() {
-	alert("Só testando se isso funciona no Github!!");
+function loadDoc(file_name) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", file_name + ".html", true);
+  xhttp.send();
 }
