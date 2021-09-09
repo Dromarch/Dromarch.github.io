73cacd2aae40ae68832a4fb8c638e59079ad5d10
unknown <Gabriel.Nascimento.2015@outlook.com>
2021-09-09 05:41:23 -0300

    update

---
+++ news.js
@@ -0,0 +1,27 @@
+var path = require('path'), fs=require('fs');
+
+function fromDir(startPath,filter){
+
+    //console.log('Starting from dir '+startPath+'/');
+
+    if (!fs.existsSync(startPath)){
+        console.log("no dir ",startPath);
+        return;
+    }
+
+    var files=fs.readdirSync(startPath);
+    for(var i=0;i<files.length;i++){
+        var filename=path.join(startPath,files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()){
+            fromDir(filename,filter); //recurse
+        }
+        else if (filename.indexOf(filter)>=0) {
+            document.write(`
+                <link rel="import" href="/news/${filename}.html">
+            `);
+        };
+    };
+};
+
+fromDir('../news','.html');
